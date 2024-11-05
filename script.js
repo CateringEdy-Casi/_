b9042238cc6826cbb68defb772cdbb7bbed345f4
CateringEdy-Casi <lucacotuna41@gmail.com>
2024-11-05 21:16:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    alert("Meniul este afișat mai jos.");
+    // Aici poți adăuga funcționalitatea pentru a derula direct la meniu sau a deschide o pagină separată
+}
+
+function showContact() {
+    alert("Pentru detalii de contact, apelați 0745 014 493.");
+    // Poți implementa alte funcționalități, precum deschiderea aplicației de apeluri sau a unui formular de contact
+}
